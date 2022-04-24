482c10a8c9fbb0cf30f0612f6ce5eb9af16a527a
Maxiboii <maksimchichkan90@gmail.com>
2022-04-24 20:13:13 +0200

    added and connected distortion to the audio chain

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -11,6 +11,65 @@
 #include <JuceHeader.h>
 
 
+template <typename Type>
+class Distortion
+{
+public:
+    //==============================================================================
+    Distortion()
+    {
+        auto& waveshaper = processorChain.template get<waveshaperIndex>();
+        waveshaper.functionToUse = [] (Type x)
+                                   {
+                                       return std::tanh (x);
+                                   };
+
+        auto& preGain = processorChain.template get<preGainIndex>();
+        preGain.setGainDecibels (50.0f);
+
+        auto& postGain = processorChain.template get<postGainIndex>();
+        postGain.setGainDecibels (0.0f);
+    }
+
+    //==============================================================================
+    void prepare (const juce::dsp::ProcessSpec& spec)
+    {
+        auto& filter = processorChain.template get<filterIndex>();
+        filter.state = FilterCoefs::makeFirstOrderHighPass (spec.sampleRate, 1000.0f);
+
+        processorChain.prepare (spec);
+    }
+
+    //==============================================================================
+    template <typename ProcessContext>
+    void process (const ProcessContext& context) noexcept
+    {
+        processorChain.process (context);
+    }
+
+    //==============================================================================
+    void reset() noexcept
+    {
+        processorChain.reset();
+    }
+
+private:
+    //==============================================================================
+    enum
+    {
+        filterIndex,
+        preGainIndex,
+        waveshaperIndex,
+        postGainIndex
+    };
+
+    using Filter = juce::dsp::IIR::Filter<Type>;
+    using FilterCoefs = juce::dsp::IIR::Coefficients<Type>;
+
+    juce::dsp::ProcessorChain<juce::dsp::ProcessorDuplicator<Filter, FilterCoefs>,
+                              juce::dsp::Gain<Type>, juce::dsp::WaveShaper<Type>, juce::dsp::Gain<Type>> processorChain;
+};
+
 enum Slope
 {
     Slope_12,
@@ -35,13 +94,14 @@ using Filter = juce::dsp::IIR::Filter<float>;
 
 using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
 
-using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, CutFilter, Filter, Distortion<float>>;
 
 enum ChainPositions
 {
     LowCut,
+    HighCut,
     Peak,
-    HighCut
+//    Distortion_
 };
 
 using Coefficients = Filter::CoefficientsPtr;
